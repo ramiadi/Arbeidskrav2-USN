298317ded9f676298c9878e4876e4e1141bdf515
ramiadi <ramin2004@hotmail.no>
2025-10-28 12:49:53 +0100

    Update arbeidskrav2.py

--- arbeidskrav2.py
+++ arbeidskrav2.py
@@ -44,6 +44,28 @@ data = {
 land = str(input("Skriv inn et land "))
 # Denne verdien henter ut landet basert på det du har skrevet i input.
 land_value = data[land]
+# Siden dictionaryen inneholder lister[], betyr det at land_value[0] (indeksen i listen) tar ut hovedstaden og land_value[1] tar ut innbyggertall
 # Hvis inputen samsvarer med dictionaryen, print ut dette
-# siden dictionaryen inneholder lister[], betyr det at land_value[0] (indeksen i listen) tar ut hovedstaden og land_value[1] tar ut innbyggertall
 print(f"{land_value[0]} er hovedstaden i {land} og det er {land_value[1]} mill. innbyggere i {land_value[0]}.")
+
+# Oppgave 5
+
+def areal_og_ytre_omkrets(a, b):
+    areal_trekant = (a * b) / 2
+    areal_halvsirkel = (np.pi * (a/2)**2) / 2 # Deles på 2 siden det er en halv sirkel.
+    omkrets_sirket = 2 * np.pi * (a/2)
+    hypotenus = np.sqrt(a**2 + b**2) 
+    bue = (2 * np.pi * (a/2) / 2) # igjen deles på 2 fordi halvdelen teller
+    omkrets_total = b + hypotenus + bue
+    areal_total = areal_trekant + areal_halvsirkel
+    return areal_total, omkrets_total
+
+# Input for å gi verdier
+a = float(input("Skriv inn grunnlinjen a: "))
+b = float(input("Skriv inn høyden b: "))
+
+# Vi gir et kall fra funksjonen
+areal, omkrets = areal_og_ytre_omkrets(a, b)
+
+print(f"Når grunnlinjen er {a} og høyden er {b}, blir det totale arealet {areal} og den ytre omkretsen {omkrets} ")
+
