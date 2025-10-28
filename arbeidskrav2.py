cfd9b36bc1241a87e8298aeec47fbb2f2fa711eb
ramiadi <ramin2004@hotmail.no>
2025-10-28 11:49:33 +0100

    Update arbeidskrav2.py

--- arbeidskrav2.py
+++ arbeidskrav2.py
@@ -4,6 +4,15 @@ Created on Tue Oct 28 11:30:11 2025
 
 @author: ramin
 """
+import math
+
 # Oppgave 1
-alder = int(input("Hvilket år er du født?"))
+alder = int(input("Hvilket år er du født? "))
 print ("Du er " + str(2024 - alder) + " år gammel")
+
+# Oppgave 2
+antall_elever = int(input("Skriv inn antall elever: "))
+# Ved å bruke math.ceil, runder vi opp tallet.
+antall_pizzaer = math.ceil(antall_elever * 0.25)
+# Bruk int slik at tallet ikke blir en desimaltall.
+print("Det må handles inn ", int(antall_pizzaer))
